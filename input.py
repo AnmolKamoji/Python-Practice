7b1b9afa2db17a30e807024ec94392715162f399
Anmol Kamoji <anmolkamoji2@gmail.com>
2025-07-29 15:06:54 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,9 @@
+a = input()
+print(a)
+# There are multiple specific data types that can be imported using the input function 
+# just add the datatype before the input like s = int(input()) so this basically takes only integer inputs. 
+# its the same for all other datatypes except the boolean datatype. For boolean it remains normal input()
+
+
+# There are also various actions that you can perform on the inputs like the following:
+
